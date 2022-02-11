4263ab258387e70d1bab491d7a431d2a0801f89f
Vadim Komarov <vadim.y.komarov@gmail.com>
2022-02-11 17:23:15 +0500

    Bump dependencies

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Autopilot.MixProject do
     [
       app: :autopilot,
       version: "0.1.0",
-      elixir: "~> 1.11",
+      elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -25,8 +25,8 @@ defmodule Autopilot.MixProject do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
       {:hound, "~> 1.1"},
-      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 1.0", only: [:dev], runtime: false}
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.1", only: [:dev], runtime: false}
     ]
   end
 end
